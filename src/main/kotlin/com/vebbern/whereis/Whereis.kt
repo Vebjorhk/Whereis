eeb2a12db38099931a404aff0e94e503c28cafcb
Vebjørn Haug Kåsene <77414086+Vebjorhk@users.noreply.github.com>
2025-12-12 16:05:11 +0100

    Add whereis command functionality

--- src/main/kotlin/com/vebbern/whereis/Whereis.kt
+++ src/main/kotlin/com/vebbern/whereis/Whereis.kt
@@ -6,6 +6,12 @@ class Whereis : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        val locateCmd = WhereisCommand() // create ONE instance
+
+        val cmd = getCommand("whereis")!!
+        cmd.setExecutor(locateCmd)
+        cmd.tabCompleter = locateCmd
+
         logger.info("Whereis is enabled")
     }
 
---
+++ src/main/kotlin/com/vebbern/whereis/WhereisCommand.kt
@@ -0,0 +1,48 @@
+package com.vebbern.whereis
+
+import org.bukkit.Bukkit
+import org.bukkit.command.*
+
+class WhereisCommand : CommandExecutor, TabCompleter {
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): Boolean {
+
+        if (args.isEmpty()) {
+            sender.sendMessage("§cUsage: /$label <player>")
+            return true
+        }
+
+        val target = Bukkit.getPlayerExact(args[0])
+        if (target == null) {
+            sender.sendMessage("§cPlayer not found.")
+            return true
+        }
+
+        val loc = target.location
+        sender.sendMessage("§a${target.name} is at ${loc.x.toInt()}, ${loc.y.toInt()}, ${loc.z.toInt()}")
+        return true
+    }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        alias: String,
+        args: Array<out String>
+    ): MutableList<String>? {
+
+        if (args.size == 1) {
+            val prefix = args[0].lowercase()
+            return Bukkit.getOnlinePlayers()
+                .map { it.name }
+                .filter { it.lowercase().startsWith(prefix) }
+                .toMutableList()
+        }
+
+        return mutableListOf()
+    }
+}
